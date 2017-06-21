8ebc9fd06296b743c03717cda2e7649b48808926
Yoseph Radding <yoseph@splt.io>
2017-06-21 00:32:42 -0400

    Finsihed Team Stuff

--- src/Models/Team.js
+++ src/Models/Team.js
@@ -1,4 +1,8 @@
-import { Model } from "../API";
+import { Model, API } from "../API";
+
+import {
+     Alert
+} from 'react-native';
 
 
 export default class Team extends Model {
@@ -10,5 +14,17 @@ export default class Team extends Model {
         return Object.assign(super.jsonFields, {
             name: String
         });
+    }
+
+    static async Match(name) {
+
+        var result = await API.get(`/teams?s=${name}`);
+        result = result.map((res) => {
+            if (res.name.toLowerCase() === name.toLowerCase()) {
+                return res;
+            }
+        });
+        // Alert.alert(result.length);
+        return result.length < 1 ? this.Create({name: name}) : new this(result[0]);
     }
 }
