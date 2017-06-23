4ee64dde62323a15a18b231e6c7524c506c37f3c
Jonathan Lehto <lehtoj21@gmail.com>
2017-06-23 15:23:40 -0400

    Scaffolding Views

---
+++ src/GroupCreate.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {User} from './Models';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert
+} from 'react-native';
+
+export default class GroupView extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({ });
