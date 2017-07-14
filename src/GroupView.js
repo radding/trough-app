0eddec7f842792cc6e9eec78884611a4ae104256
Jonathan Lehto <lehtoj21@gmail.com>
2017-07-14 09:42:04 -0400

    Style the GroupView

--- src/GroupView.js
+++ src/GroupView.js
@@ -31,22 +31,32 @@ class Group extends Component {
                 {/*navigate("")*/}
                 console.warn("Not implemented");
             } } style={styles.row} >
-                <Text>{this.props.group.creator.name} is going to {this.props.group.place.name}.</Text>
-                <Text>Want to Join?</Text>
-                <View>
-                    <Button 
-                        title="No"
-                        onPress={() => this.props.remove(this.props.group)}
-                    />
-                    <Button 
-                        title="Yes"
-                        onPress={() => {
-                            this.props.add(this.props.group);
-                            this.props.remove(this.props.group);
-                        }}
-                    />
+                <View style={{flexDirection: "column"}}>
+                    <View style={{backgroundColor: "#CCC"}}>
+                        <Text style={{padding: 10, fontSize: 18}}>{this.props.group.name}</Text>
+                    </View>
+                    <View style={{padding: 10, backgroundColor: "#FFF"}}>
+                        <Text style={{fontSize: 16}}>{this.props.group.users.length} people are going to {this.props.group.place.name}, at {this.props.group.departure}.  Select to see who is going.</Text>
+                    </View>
+                    <View style={{width: "100%", paddingTop: 10, flexDirection: "row", backgroundColor: "#FFF"}}>
+                        <View style={{flex: 1}}>
+                            <Button 
+                                title="Join This Outing"
+                                onPress={() => {
+                                    this.props.add(this.props.group);
+                                    this.props.remove(this.props.group);
+                                }}
+                                style={{borderRightWidth: 1, borderColor: "#000"}}
+                            />
+                        </View>
+                        <View style={{flex: 1}}>
+                            <Button 
+                                title="Hide This Outing"
+                                onPress={() => this.props.remove(this.props.group)}
+                            />
+                        </View>
+                    </View>
                 </View>
-
             </TouchableOpacity>
         )
 
@@ -86,7 +96,7 @@ class GroupView extends Component {
         return (
             
             <View>
-                <Text>There are no Group outings, Create one now</Text>
+                <Text style={{fontSize: 18, margin: 10}}>There are no Group outings, Create one now</Text>
                 <Button 
                     title="Create an Outing"
                     onPress={() => {
@@ -100,13 +110,15 @@ class GroupView extends Component {
     _renderAll = () => {
         const { navigate } = this.props.navigation;      
         return (
-            <View>
-                <Button 
-                    title="Create an Outing"
-                    onPress={() => {
-                        navigate("create");
-                    }}
-                />
+            <View style={{backgroundColor: "#f0f8ff"}}>
+                <View style={{marginTop: 30}}>    
+                    <Button 
+                        title="Create an Outing"
+                        onPress={() => {
+                            navigate("create");
+                        }}
+                    />
+                </View>
                 <SwipeListView 
                     dataSource={this.ds.cloneWithRows(this.state.outings)}
                     renderRow={(group, secId, rowId, rowMap) => 
@@ -180,7 +192,8 @@ export default connect(mapStateToProps, mapDispatchToProps)(GroupView);
 
 const styles = {
     row: {
-        backgroundColor: "white"
+        backgroundColor: "#f0f8ff",
+        paddingTop: 30
     },
     rowBack: {
 		alignItems: 'center',
